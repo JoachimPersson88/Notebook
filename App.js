6551b116dafb372d2b18e6ed48bb319033822008
Joachim Persson <joachim.persson88@gmail.com>
2023-08-26 16:48:47 +0200

    fått en stabil grund till projektet, nu ska det mer eller mindre bara finslippas

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import NotesListScreen from './screens/NotesListScreen';
+import NoteEditorScreen from './screens/NoteEditorScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="NotesList">
+        <Stack.Screen name="NotesList" component={NotesListScreen} />
+        <Stack.Screen name="NoteEditor" component={NoteEditorScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
